d750719d305f7dda714ce93242bed7b65cda5263
Evgeniya D. <Evgeniya7000@rambler.ru>
2024-02-01 20:51:25 +1200

    Changed to camal case

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,18 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    int money_start = 100;
-    int money_add = 2001;
-    int bonus_price = 100;
-    int bonus_amount;
-    int money_end;
-    if (money_add >= 100){
-        bonus_amount = money_add / bonus_price;
-    }else{
-        bonus_amount = 0;
-    }
-    money_end = money_start + money_add + bonus_amount;
-    System.out.println("Начислено бонусных рублей: " + bonus_amount);
-    System.out.println("Итого остаток на счете: " + money_end);
+        int moneyStart = 100;
+        int moneyAdd = 2001;
+        int bonusPrice = 100;
+        int bonusAmount;
+        int moneyEnd;
+        if (moneyAdd >= 100) {
+            bonusAmount = moneyAdd / bonusPrice;
+        } else {
+            bonusAmount = 0;
+        }
+        moneyEnd = moneyStart + moneyAdd + bonusAmount;
+        System.out.println("Начислено бонусных рублей: " + bonusAmount);
+        System.out.println("Итого остаток на счете: " + moneyEnd);
     }
 }
